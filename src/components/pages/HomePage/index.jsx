9b37aa1b98e422cfd68d0f24dba3e4f818c1d3c9
Christian Aichner <contact@aichner-christian.com>
2020-03-24 22:24:30 +0100

    Improve design
    The design and content of the homepage has been
    improved.

--- src/components/pages/HomePage/index.jsx
+++ src/components/pages/HomePage/index.jsx
@@ -20,12 +20,24 @@ import {
   MDBMask,
   MDBInput,
   MDBBtn,
+  MDBFormInline,
 } from "mdbreact";
 
 //> Images
 import shopIMG from "../../../assets/content/shops.jpg";
+import partnerIMG from "../../../assets/content/vertriebspartner.jpg";
 
 class HomePage extends React.Component {
+  state = {
+    radio: 2
+  };
+
+  onClick = nr => () => {
+    this.setState({
+      radio: nr
+    });
+  };
+
   render() {
     return (
       <div id="hero">
@@ -37,7 +49,8 @@ class HomePage extends React.Component {
           >
             <MDBRow className="d-flex justify-content-center">
               <MDBCol md="6" className="white-text text-center text-md-left mb-5">
-                <h1 className="display-4 font-weight-bold text-uppercase">Natur Vertrieb</h1>
+                <h1 className="display-4 font-weight-bold">naturvertrieb.at</h1>
+                <h3>Where nature meets quality.</h3>
                 <hr className="hr-light" />
                 <h6 className="mb-4">
                   Unmöglich kann ein naturhaftes Begehren vergeblich sein.
@@ -46,15 +59,71 @@ class HomePage extends React.Component {
                   <MDBIcon icon="users" />
                   Partner werden
                 </MDBBtn>
-                <MDBBtn color="secondary" size="lg">
-                  <MDBIcon icon="shopping-cart" />
-                  Jetzt shoppen
+                <MDBBtn color="green" size="lg">
+                  <MDBIcon far icon="envelope" />
+                  Kontakt
                 </MDBBtn>
               </MDBCol>
               <MDBCol md="6" className="mb-4">
-                <MDBCard className="dark-grey-text">
+                <MDBCard className="text-center">
                   <MDBCardBody>
-                    
+                    <h2 className="font-weight-bold">Treten Sie der Warteliste bei!</h2>
+                    <p className="lead">
+                    Immer mehr lokale Betriebe und Direktvermarkter nutzen ihre einzigartige Chance auf unserer 
+                    Plattform.
+                    </p>
+                    <p>
+                    Finde auch Du heraus, wie du im Naturvertrieb teilnehmen kannst um Deine tollen Produkte zu 
+                    verkaufen oder Direktvertrieb zu betreiben.
+                    </p>
+                    <h3 className="mt-4 mb-2">Entdecke die Vorteile</h3>
+                    <p className="mb-0">
+                    Du bist Betreiber eines Betriebs und willst im profitablen Online-Markt Deine Produkte vertreiben?
+                    </p>
+                    <MDBBtn color="green">
+                      <MDBIcon icon="chart-line"/>
+                      Shop eröffnen
+                    </MDBBtn>
+                    <p className="mt-3 mb-0">
+                    Du bist Direktvermarkter und willst durch Vermarktung und 
+                    Weiterempfehlungen profitieren?
+                    </p>
+                    <MDBBtn color="elegant">
+                      <MDBIcon icon="handshake"/>
+                      Vertriebspartner werden
+                    </MDBBtn>
+                    <div className="mt-4">
+                    <hr />
+                      <p className="lead mt-4">
+                      Trete <strong>JETZT</strong> unserer Warteliste bei und sichere Dir Deinen Platz auf 
+                      naturvertrieb.at!
+                      </p>
+                      <form>
+                        <input className="form-control" type="email" name="email" placeholder="Deine E-Mail" required />
+                        <div className="text-left my-3">
+                          <MDBInput
+                            label={
+                              <p>Ich habe die <a 
+                              href="https://www.naturvertrieb.at/privacy"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              >
+                              Datenschutzerklärung
+                              </a> gelesen und stimme der Weiterverarbeitung meiner Daten für die Kontaktaufnahme zu.
+                              </p>
+                            }
+                            filled
+                            type="checkbox"
+                            id="checkbox1"
+                            required
+                            containerClass="mr-5"
+                          />
+                        </div>
+                        <MDBBtn color="secondary" size="lg" type="submit">
+                        Warteliste beitreten
+                        </MDBBtn>
+                      </form>
+                    </div>
                   </MDBCardBody>
                 </MDBCard>
               </MDBCol>
@@ -62,47 +131,59 @@ class HomePage extends React.Component {
           </MDBContainer>
         </MDBView>
         <MDBContainer>
-          <MDBRow className="py-5">
+        <h2 className="h1 font-weight-bold text-center mt-5">Wie Sie profitieren</h2>
+          <MDBRow className="pb-5 mt-4">
             <MDBCol md="5" className="text-center">
               <img src={shopIMG} alt="" className="img-fluid"/>
             </MDBCol>
             <MDBCol md="7">
+              <h2>Verkaufen Sie Produkte online</h2>
+              <p className="lead">
+              Der moderne Online-Markt bietet sehr viele Vorteile und eine gigantische Menge an Neukunden.
+              </p>
+              <p className="lead font-weight-bold mb-0">
+              Wir betreiben Ihren Shop!
+              </p>
               <p>
-                Sie betreieben keinen <strong>eigenen Onlineshop</strong> mit Ihren <strong>Naturprodukten</strong>, 
-                möchten aber den Verkauf Ihrer Produkte steigern?</p> 
-              <p>
-                Werden Sie <strong>JETZT</strong> Partner und stellen Sie Ihre Produkte in unseren Onlineshop und 
+              Sie haben daher wenig Selbstaufwand und können unproblematisch Ihre Produkte verkaufen. Wir übernehmen das
+              Hosting und Betreuung des Shops.
+              </p>
+              <p className="lead font-weight-bold mb-0">
+              Steigern Sie Ihrem Umsatz!
+              </p>
+              <p>Werden Sie JETZT Partner und stellen Sie Ihre Produkte in unseren Onlineshop und 
                 steigern Sie somit Ihren Umastz.
               </p>
               <p>
                 <MDBBtn
-                color="secondary"
+                color="green"
                 >
                   Jetzt Online-Shop eröffnen
                 </MDBBtn>
               </p>
             </MDBCol>
-            <MDBCol md="8" className="text-center">
+            <MDBCol md="12" className="py-3" />
+            <MDBCol md="7" className="text-right">
+              <h2>Vertreiben Sie Produkte</h2>
+              <p className="lead">
+              Wollen Sie unkomplizert in Online Marketing einsteigen und Ihren eigenen Vertrieb aufbauen?
+              </p>
+              <p className="lead font-weight-bold mb-0">
+              Sie vertreiben, wir belohnen!
+              </p>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Sie werden nach unserem Vergütungsplan entsprechend entlohnt.
+              </p>
+              <p>
+                <MDBBtn
+                color="elegant"
+                >
+                  Vertriebspartner werden
+                </MDBBtn>
               </p>
             </MDBCol>
-            <MDBCol md="4" className="text-center">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
+            <MDBCol md="5" className="text-center">
+              <img src={partnerIMG} alt="" className="img-fluid"/>
             </MDBCol>
           </MDBRow>
         </MDBContainer>
