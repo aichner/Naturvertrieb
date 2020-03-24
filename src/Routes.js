11916bddfd0cef103b79ea16f0434e028822ecbb
Christian Aichner <contact@aichner-christian.com>
2020-03-24 22:13:51 +0100

    Add messagepage
    The page to display any kind of information based on
    the url has been added.

--- src/Routes.js
+++ src/Routes.js
@@ -9,7 +9,8 @@ import { Route, Switch } from 'react-router-dom';
  * HomePage: A basic template page
  */
 import {
-  HomePage
+  HomePage,
+  MessagePage,
 } from './components/pages';
 
 class Routes extends React.Component {
@@ -17,12 +18,22 @@ class Routes extends React.Component {
     return (
       <Switch>
         <Route exact path='/' component={HomePage} />
-          
+        <Route 
+        exact
+        path='/about'
+        component={(props) => <MessagePage {...props}/>}
+        />
+        <Route 
+        exact
+        path='/privacy'
+        component={(props) => <MessagePage {...props}/>}
+        />
         <Route
           render={function () {
             return <h1>Not Found</h1>;
           }}
         />
+        
       </Switch>
     );
   }
