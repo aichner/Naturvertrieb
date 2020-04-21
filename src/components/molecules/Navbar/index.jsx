7514a2310c2e72ac06dd3cc7b318930cadf09858
Christian Aichner <contact@aichner-christian.com>
2020-04-21 19:47:01 +0200

    Improve navbar
    The navigation bar has been updated to include more 
    functionality.

--- src/components/molecules/Navbar/index.jsx
+++ src/components/molecules/Navbar/index.jsx
@@ -1,6 +1,8 @@
 //> React
 // Contains all the functionality necessary to define React components
 import React from "react";
+// Router
+import { withRouter } from "react-router-dom";
 
 //> MDB
 // "Material Design for Bootstrap" is a great UI design framework
@@ -13,7 +15,14 @@ import {
   MDBNavItem,
   MDBNavLink,
   MDBContainer,
+  MDBBtn,
 } from "mdbreact";
+
+//> Redux
+// Connect
+import { connect } from "react-redux";
+// Actions
+import { signOut } from "../../../store/actions/authActions";
 
 //> CSS
 import "./navbar.scss";
@@ -44,40 +53,90 @@ class Navbar extends React.Component {
         onClick={this.toggleCollapse("mainNavbarCollapse")}
       />
     );
-
     const { collapseID } = this.state;
-    return (
-      <div>
-        <MDBNavbar color="white" light expand="md" fixed="top" scrolling>
-          <MDBContainer>
-            <MDBNavbarBrand href="/" className="py-0 font-weight-bold">
-              <img src={Logo} alt="Naturvertrieb" className="img-fluid" />
-            </MDBNavbarBrand>
-            <MDBNavbarToggler
-              onClick={this.toggleCollapse("mainNavbarCollapse")}
-            />
-            <MDBCollapse
-              id="mainNavbarCollapse"
-              isOpen={this.state.collapseID}
-              navbar
-            >
-              <MDBNavbarNav right>
-                <MDBNavItem>
-                  <a href="mailto:info@naturvertrieb.at" className="text-dark">
-                    <strong>Kontakt</strong>
-                  </a>
-                </MDBNavItem>
-              </MDBNavbarNav>
-            </MDBCollapse>
-          </MDBContainer>
-        </MDBNavbar>
-        {collapseID && overlay}
-      </div>
-    );
+    const { auth, profile, location } = this.props;
+
+    if (location && location.pathname !== "/manage") {
+      return (
+        <div>
+          <MDBNavbar color="white" light expand="md" fixed="top" scrolling>
+            <MDBContainer>
+              <MDBNavbarBrand href="/" className="py-0 font-weight-bold">
+                <img src={Logo} alt="Naturvertrieb" className="img-fluid" />
+              </MDBNavbarBrand>
+              <MDBNavbarToggler
+                onClick={this.toggleCollapse("mainNavbarCollapse")}
+              />
+              <MDBCollapse
+                id="mainNavbarCollapse"
+                isOpen={this.state.collapseID}
+                navbar
+              >
+                <MDBNavbarNav right className="flex-center">
+                  <MDBNavItem className="mr-2">
+                    <a
+                      href="mailto:info@naturvertrieb.at"
+                      className="text-dark"
+                    >
+                      <strong>Kontakt</strong>
+                    </a>
+                  </MDBNavItem>
+                  {auth.uid && (
+                    <MDBNavItem>
+                      <MDBNavLink
+                        exact
+                        to=""
+                        onClick={() => this.props.signOut()}
+                      >
+                        <MDBBtn size="lg" outline color="elegant">
+                          Logout
+                        </MDBBtn>
+                      </MDBNavLink>
+                    </MDBNavItem>
+                  )}
+                  {!auth.uid && (
+                    <>
+                      <MDBNavItem>
+                        <MDBNavLink exact to="questionnaire">
+                          <MDBBtn size="lg" color="green">
+                            Fragebogen
+                          </MDBBtn>
+                        </MDBNavLink>
+                      </MDBNavItem>
+                      <MDBNavItem>
+                        <MDBNavLink exact to="login">
+                          <MDBBtn size="lg" color="elegant" outline>
+                            Login
+                          </MDBBtn>
+                        </MDBNavLink>
+                      </MDBNavItem>
+                    </>
+                  )}
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBContainer>
+          </MDBNavbar>
+          {collapseID && overlay}
+        </div>
+      );
+    } else {
+      return null;
+    }
   }
 }
 
-export default Navbar;
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+    profile: state.firebase.profile,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signOut: () => dispatch(signOut()),
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Navbar));
 
 /**
  * SPDX-License-Identifier: (EUPL-1.2)
