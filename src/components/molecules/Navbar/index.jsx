b0514e72b291cd51a74095fcd586b581c00692a7
Christian Aichner <contact@aichner-christian.com>
2020-04-21 19:12:54 +0200

    Update version
    The version has been updated to 2019-2020.

--- src/components/molecules/Navbar/index.jsx
+++ src/components/molecules/Navbar/index.jsx
@@ -81,5 +81,5 @@ export default Navbar;
 
 /**
  * SPDX-License-Identifier: (EUPL-1.2)
- * Copyright © 2019 Werbeagentur Christian Aichner
+ * Copyright © 2019-2020 Werbeagentur Christian Aichner
  */
