565f7b7e67b288591d961d12eb6756a9ab506457
Christian Aichner <contact@aichner-christian.com>
2020-04-21 19:00:43 +0200

    Implement Login and Profile page
    The template login and Profile page have been added.

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -2,13 +2,17 @@
 // Import all components to export them for easy access from parent components
 import HomePage from "./HomePage";
 import MessagePage from "./MessagePage";
+import LoginPage from "./LoginPage";
+import ProfilePage from "./ProfilePage";
 
 export { 
   HomePage,
   MessagePage,
+  LoginPage,
+  ProfilePage,
 };
 
 /**
  * SPDX-License-Identifier: (EUPL-1.2)
- * Copyright © 2019 Werbeagentur Christian Aichner
+ * Copyright © 2019-2020 Werbeagentur Christian Aichner
  */
