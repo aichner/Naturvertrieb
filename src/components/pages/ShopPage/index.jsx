46d4073346fc93f70dc3b5ff00b6b97bbc4da5c5
Christian Aichner <contact@aichner-christian.com>
2020-04-29 23:44:37 +0200

    Add content to ShopPage
    The basic React Router match param handling has been implemented.

--- src/components/pages/ShopPage/index.jsx
+++ src/components/pages/ShopPage/index.jsx
@@ -1,6 +1,8 @@
 //> React
 // Contains all the functionality necessary to define React components
 import React from "react";
+// Router DOM bindings
+import { withRouter } from "react-router-dom";
 
 //> MDB
 // "Material Design for Bootstrap" is a great UI design framework
@@ -8,6 +10,22 @@ import { MDBContainer } from "mdbreact";
 
 class ShopPage extends React.Component {
   state = {};
+
+  componentDidMount = () => {
+    if (
+      this.props.match &&
+      this.props.match &&
+      this.props.match.params &&
+      this.props.match.params.shop
+    ) {
+      // Get shop name for query
+      const shop = this.props.match.params.shop;
+
+      // Do query to get shop data
+    } else {
+      // Shop not found
+    }
+  };
 
   render() {
     return (
@@ -18,7 +36,7 @@ class ShopPage extends React.Component {
   }
 }
 
-export default ShopPage;
+export default withRouter(ShopPage);
 
 /**
  * SPDX-License-Identifier: (EUPL-1.2)
