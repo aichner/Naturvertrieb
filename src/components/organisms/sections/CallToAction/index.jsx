f2f60452972043a7df34f0967f10948cf1c035cb
Christian Aichner <contact@aichner-christian.com>
2020-04-30 12:00:56 +0200

    Improve Call To Action
    The call to action has been improved and an alternative for the box text has been added.

--- src/components/organisms/sections/CallToAction/index.jsx
+++ src/components/organisms/sections/CallToAction/index.jsx
@@ -1,6 +1,8 @@
 //> React
 // Contains all the functionality necessary to define React components
 import React from "react";
+// Router DOM bindings
+import { Link } from "react-router-dom";
 
 //> MDB
 // "Material Design for Bootstrap" is a great UI design framework
@@ -27,21 +29,40 @@ import {
 //> CSS
 import "./calltoaction.scss";
 
+//> Config
+const box = false;
+
 class CallToAction extends React.Component {
   render() {
     return (
       <section id="calltoaction">
-        <MDBContainer className="text-center">
-          <h2>Box online zusammenstellen</h2>
-          <p className="lead">
-            Heute zusammenstellen und in den nächsten Tagen direkt an Ihre
-            Haustüre geliefert erhalten.
-          </p>
-          <MDBBtn color="green" size="lg">
-            <MDBIcon icon="box" />
-            Box zusammenstellen
-          </MDBBtn>
-        </MDBContainer>
+        {box ? (
+          <MDBContainer className="text-center">
+            <h2>Box online zusammenstellen</h2>
+            <p className="lead">
+              Heute zusammenstellen und in den nächsten Tagen direkt vor Deine
+              Haustüre geliefert erhalten.
+            </p>
+            <MDBBtn color="green" size="lg">
+              <MDBIcon icon="box" />
+              Box zusammenstellen
+            </MDBBtn>
+          </MDBContainer>
+        ) : (
+          <MDBContainer className="text-center">
+            <h2>Lokale Produkte erkunden</h2>
+            <p className="lead">
+              Heute auswählen und in den nächsten Tagen direkt vor Deine
+              Haustüre geliefert erhalten.
+            </p>
+            <Link to="/products">
+              <MDBBtn color="green" size="lg">
+                <MDBIcon icon="list-alt" />
+                Zum Sortiment
+              </MDBBtn>
+            </Link>
+          </MDBContainer>
+        )}
       </section>
     );
   }
