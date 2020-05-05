d93ac53053ff12786c5f0664db127e53fe412033
Christian Aichner <contact@aichner-christian.com>
2020-05-06 01:24:10 +0200

    Improve footer
    The design and content of the footer has been improved.

--- src/components/molecules/Footer/index.jsx
+++ src/components/molecules/Footer/index.jsx
@@ -10,64 +10,127 @@ import copy from "copy-to-clipboard";
 
 //> MDB
 // "Material Design for Bootstrap" is a great UI design framework
-import { MDBFooter, MDBRow, MDBCol, MDBContainer, MDBIcon } from "mdbreact";
+import {
+  MDBFooter,
+  MDBRow,
+  MDBCol,
+  MDBContainer,
+  MDBBtn,
+  MDBIcon,
+} from "mdbreact";
 
 //> CSS
 import "./footer.scss";
 
 //> Images
 import logoImg from "../../../assets/content/h120.png";
+import payPalImg from "../../../assets/content/trusted/paypal.png";
+import masterCardImg from "../../../assets/content/trusted/mastercard.png";
+import visaCardImg from "../../../assets/content/trusted/visa.png";
+import sofortImg from "../../../assets/content/trusted/sofort.png";
 
 class Footer extends React.Component {
   state = {};
   render() {
     return (
-      <MDBFooter color="secondary" className="font-small">
+      <MDBFooter color="white" className="font-small text-dark pt-4">
         <MDBContainer className="text-center text-md-left py-3">
-          <MDBRow className="flex-center">
+          <MDBRow>
+            <MDBCol md="6" className="text-left">
+              <p className="font-weight-bold lead">Zahlungsarten</p>
+              <div className="trusted">
+                <img src={payPalImg} alt="PayPal" className="img-fluid" />
+                <img
+                  src={sofortImg}
+                  alt="Sofortüberweisung"
+                  className="img-fluid"
+                />
+                <img src={visaCardImg} alt="Visa" className="img-fluid" />
+                <img
+                  src={masterCardImg}
+                  alt="Mastercard"
+                  className="img-fluid"
+                />
+              </div>
+            </MDBCol>
+            <MDBCol md="6" className="text-left">
+              <p className="font-weight-bold lead">Bei uns kaufen Sie sicher</p>
+              <div className="trusted">
+                <p className="text-muted">Gütezeichen to be added</p>
+              </div>
+            </MDBCol>
+          </MDBRow>
+          <MDBRow className="flex-center mt-3 align-items-initial">
             <MDBCol md="3" className="text-center">
+              <p className="font-weight-bold lead text-left mb-2">
+                Rechtliches
+              </p>
               <Link to="about">
                 <li className="list-unstyled">Impressum</li>
               </Link>
-              {/*<a
-            href="mailto:support@aichner-christian.com"
-            >
-              <li className="list-unstyled">
-                Datenschutzerklärung
-              </li>
-            </a>*/}
+              <Link to="privacy">
+                <li className="list-unstyled">Datenschutzerklärung</li>
+              </Link>
+              <Link to="anb">
+                <li className="list-unstyled">Nutzungsbedingungen</li>
+              </Link>
+              <Link to="disclaimer">
+                <li className="list-unstyled">Widerruf</li>
+              </Link>
             </MDBCol>
-            <MDBCol md="2">
+            <MDBCol md="3" className="text-center">
+              <p className="font-weight-bold lead text-left mb-2">Verkaufen</p>
+              <Link to="partnershop">
+                <li className="list-unstyled">Shop eröffnen</li>
+              </Link>
+              <Link to="partner">
+                <li className="list-unstyled">Affiliate Partner werden</li>
+              </Link>
+              <Link to="partnerlist">
+                <li className="list-unstyled">Unsere Partner</li>
+              </Link>
+              <Link to="faq">
+                <li className="list-unstyled">FAQ</li>
+              </Link>
+            </MDBCol>
+            <MDBCol md="3" className="text-center">
+              <p className="font-weight-bold lead text-left mb-2">Kategorien</p>
+              <li className="list-unstyled">Nahrungsergänzungsmittel</li>
+              <li className="list-unstyled">Lebensmittel</li>
+              <li className="list-unstyled">Körperpflege</li>
+              <li className="list-unstyled">Kleidung</li>
+            </MDBCol>
+            <MDBCol md="3" className="text-left">
+              <p className="font-weight-bold lead text-left mb-2">
+                Kundensupport
+              </p>
+              <p>MO-SO 9:00 - 18:00</p>
+              <p>+43 660 5752112</p>
+              <p className="font-weight-bold mt-3">Kontakt über Facebook</p>
+              <div>
+                <MDBBtn color="blue" className="w-100 m-0">
+                  <MDBIcon fab icon="facebook-messenger" />
+                  Facebook Messenger
+                </MDBBtn>
+              </div>
+              <p className="font-weight-bold mt-3">Kontakt per E-Mail</p>
+              <div>
+                <MDBBtn color="elegant" className="w-100 m-0">
+                  <MDBIcon far icon="envelope" />
+                  E-Mail
+                </MDBBtn>
+              </div>
+            </MDBCol>
+            <MDBCol md="2" className="mt-4 mb-2">
               <img
                 src={logoImg}
                 alt="Naturvertrieb Logo"
                 className="img-fluid"
               />
             </MDBCol>
-            <MDBCol md="3" className="text-center">
-              <a href="mailto:info@naturvertrieb.at">
-                <li className="list-unstyled">Kontakt</li>
-              </a>
-              {this.state.copied ? (
-                <li className="list-unstyled success-color">
-                  <MDBIcon icon="check" className="mr-2" />
-                  Link kopiert
-                </li>
-              ) : (
-                <a
-                  onClick={() => {
-                    this.setState({ copied: true }, () =>
-                      copy("https://www.naturvertrieb.at")
-                    );
-                  }}
-                >
-                  <li className="list-unstyled">Teilen</li>
-                </a>
-              )}
-            </MDBCol>
           </MDBRow>
         </MDBContainer>
-        <div className="footer-copyright text-center py-3">
+        <div className="footer-copyright text-center py-3 text-dark">
           <MDBContainer fluid>
             <div>
               &copy; {new Date().getFullYear()} Copyright: Vertrieb Luca Egger
@@ -81,7 +144,7 @@ class Footer extends React.Component {
                 <a
                   href="https://www.aichner-christian.com"
                   target="_blank"
-                  className="ml-1"
+                  className="ml-1 text-dark underlined"
                   rel="noopener noreferrer"
                 >
                   Werbeagentur Christian Aichner
@@ -99,6 +162,7 @@ class Footer extends React.Component {
                   href="https://github.com/aichner/Naturvertrieb"
                   rel="noopener noreferrer"
                   target="_blank"
+                  className="text-dark"
                 >
                   <MDBIcon fab icon="github" className="pr-2" />
                   View on GitHub
@@ -108,6 +172,7 @@ class Footer extends React.Component {
                   href="https://github.com/aichner/Naturvertrieb/issues/new?template=bug_report.md"
                   rel="noopener noreferrer"
                   target="_blank"
+                  className="text-dark"
                 >
                   <MDBIcon icon="bug" className="pr-2" />
                   Report bug
